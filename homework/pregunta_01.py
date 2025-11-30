65a5024b793870ea32b070e49f1d4b5043dc8dc0
Tyrussgc <141864114+Tyrussgc@users.noreply.github.com>
2025-11-29 22:15:47 -0500

    LISTO

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -6,6 +6,10 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 
+from pathlib import Path  #? Importa la clase Path de la librería pathlib para facilitar la manipulación de rutas de archivos y directorios.
+import pandas as pd  #? Importa la librería pandas para trabajar con estructuras de datos (DataFrame) en Python.
+from zipfile import ZipFile  #? Importa la clase ZipFile de la librería zipfile para manejar archivos comprimidos (.zip).
+
 def pregunta_01():
     """
     La información requerida para este laboratio esta almacenada en el
@@ -71,3 +75,20 @@ def pregunta_01():
 
 
     """
+    # Abrir y descomprimir el archivo zip
+    with ZipFile("files/input.zip", "r") as z:  #? Abre el archivo zip ubicado en "files/input.zip" en modo lectura ("r").
+        z.extractall("files/")  #? Extrae todos los archivos del archivo zip al directorio "files/".
+
+    direccion_input = Path("files/input")  #? Define la ruta donde se encuentra la carpeta "input" después de la descompresión.
+    direccion_output = Path("files/output")  #? Define la ruta donde se guardarán los archivos de salida (en "files/output").
+    direccion_output.mkdir(parents=True, exist_ok=True)  #? Crea la carpeta "output" si no existe, incluyendo las carpetas padre si es necesario.
+
+    for dataset in ["train", "test"]:  #? Itera sobre las carpetas "train" y "test", que contienen las subcarpetas con los archivos de texto.
+        frases = []  #? Inicializa una lista vacía para almacenar las frases y sus etiquetas (sentimiento).
+        for target_path in (direccion_input / dataset).iterdir():  #? Itera sobre los directorios dentro de "train" o "test".
+            for file_path in target_path.iterdir():  #? Itera sobre los archivos de cada subdirectorio (por ejemplo, los archivos de texto de "negative", "positive" o "neutral").
+                frase = file_path.read_text()  #? Lee el contenido del archivo de texto.
+                frases.append((frase, target_path.name))  #? Añade una tupla con la frase y su etiqueta (el nombre del directorio, que es el sentimiento).
+        
+        df = pd.DataFrame(frases, columns=["phrase", "target"])  #? Crea un DataFrame de pandas con las frases y sus respectivas etiquetas.
+        df.to_csv(direccion_output / f"{dataset}_dataset.csv")  #? Guarda el DataFrame como un archivo CSV en el directorio "output" con el nombre "train_dataset.csv" o "test_dataset.csv".
